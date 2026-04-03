3bd22314af27aeb31fdff5d89ab4659cfbf9d85b
ADMIN <NGUYENTHANGBE04@GMAIL.COM>
2026-04-03 14:34:21 +0700

    update

--- FashionStore/Areas/Admin/Controllers/AccountController.cs
+++ FashionStore/Areas/Admin/Controllers/AccountController.cs
@@ -1,11 +1,8 @@
 ﻿using FashionStore.Repository;
 using FashionStore.Utilities;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace FashionStore.Areas.Admin.Controllers
 {
@@ -13,6 +10,8 @@ namespace FashionStore.Areas.Admin.Controllers
     public class AccountController : Controller
     {
         private readonly fashionDbContext _context;
+        private static Dictionary<string, (int Count, DateTime LockUntil)> loginAttempts
+            = new Dictionary<string, (int, DateTime)>();
 
         public AccountController(fashionDbContext context)
         {
@@ -25,49 +24,80 @@ namespace FashionStore.Areas.Admin.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken] 
         public async Task<IActionResult> Login(string Username, string Password)
         {
-            var user = _context.Users.FirstOrDefault(u => u.Username == Username && u.Role == "Admin");
+            var ip = HttpContext.Connection.RemoteIpAddress?.ToString();
+            var key = $"{Username}_{ip}";
+            if (loginAttempts.ContainsKey(key))
+            {
+                var info = loginAttempts[key];
+                if (info.LockUntil > DateTime.Now)
+                {
+                    ViewBag.Error = "Too many attempts. Try again in 5 minutes.";
+                    return View();
+                }
+            }
+
+            var user = _context.Users
+                .FirstOrDefault(u => u.Username == Username && u.Role == "Admin");
 
             if (user == null)
             {
+                IncreaseFail(key);
                 ViewBag.Error = "Not Admin";
                 return View();
             }
-
             if (!PasswordHelper.Verify(user.PasswordHash, Password))
             {
                 string md5 = PasswordHelper.GetMD5(Password);
 
                 if (user.PasswordHash != md5)
                 {
+                    IncreaseFail(key);
                     ViewBag.Error = "Incorrect password!";
                     return View();
                 }
-
-                // upgrade
                 user.PasswordHash = PasswordHelper.Hash(Password);
                 _context.SaveChanges();
             }
-            if (user == null)
-            {
-                ViewBag.Error = "Not Admin";
-                return View();
-            }
+            loginAttempts.Remove(key);
 
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.FullName ?? string.Empty),
-                new Claim(ClaimTypes.Role, user.Role ?? string.Empty),
+                new Claim(ClaimTypes.Name, user.FullName ?? ""),
+                new Claim(ClaimTypes.Role, user.Role ?? ""),
                 new Claim("UserId", user.UserId.ToString())
             };
 
             var identity = new ClaimsIdentity(claims, "AdminScheme");
+
             await HttpContext.SignInAsync(
                 "AdminScheme",
                 new ClaimsPrincipal(identity));
 
             return RedirectToAction("Index", "ProductAdmin");
+        }
+
+        private void IncreaseFail(string key)
+        {
+            if (!loginAttempts.ContainsKey(key))
+            {
+                loginAttempts[key] = (1, DateTime.MinValue);
+            }
+            else
+            {
+                var current = loginAttempts[key];
+                current.Count++;
+
+                if (current.Count >= 5)
+                {
+                    current.LockUntil = DateTime.Now.AddMinutes(5);
+                    current.Count = 0;
+                }
+
+                loginAttempts[key] = current;
+            }
         }
     }
 }
